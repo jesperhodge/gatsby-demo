ce32f32da1516096a236f480930b0dcb1e43400a
Jesper Hodge <jesper@instaffo.de>
2019-08-20 15:41:36 +0200

    make expanding tabs

--- src/pages/frontpage.js
+++ src/pages/frontpage.js
@@ -20,15 +20,19 @@ const color3 = 'rgba(2, 2, 2, 1)'
 const color4 = 'rgba(232, 219, 125, 1)'
 const color5 = 'rgba(239, 247, 255, 1)'
 
-export default () => (
+const expanseStyle = (color) => ({ color: 'white', backgroundColor: color, display: 'inline-block', height: '100%', transition: 'width .8s' })
+
+export default () => {
+  const [selectedTab, setSelectedTab] = useState(1)
+
+  return (
   <div style={containerStyle} >
     <h1 style={headerStyle} >Page</h1>
     <div style={{height: '400px', marginTop: '300px'}}>
-      <p style={{ backgroundColor: color1, display: 'inline-block', width: '600px', height: '100%' }} />
-      <p style={{ backgroundColor: color2, display: 'inline-block', width: '100px', height: '100%' }} />
-      <p style={{ backgroundColor: color4, display: 'inline-block', width: '100px', height: '100%' }} />
-      <p style={{ backgroundColor: color5, display: 'inline-block', width: '100px', height: '100%' }} />
+      <p style={expanseStyle(color1)} className={selectedTab === 1 ? 'wide' : 'narrow'} onMouseOver={() => {setSelectedTab(1)}}>Projects</p>
+      <p style={expanseStyle(color2)} className={selectedTab === 2 ? 'wide' : 'narrow'} onMouseOver={() => {setSelectedTab(2)}}>Stuff</p>
+          <p style={expanseStyle(color4)} className={selectedTab === 4 ? 'wide' : 'narrow'} onMouseOver={() => {setSelectedTab(4)}}>Life</p>
+          <p style={expanseStyle(color5)} className={selectedTab === 5 ? 'wide' : 'narrow'} onMouseOver={() => {setSelectedTab(5)}}>Things</p>
     </div>
-    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZDLK6YsNPqIJW7_wJDqeD58huaG4_YVLEfsG48GP2_GgZDKSd" />
   </div>
-)
+)}
